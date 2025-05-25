57fa8241b2c2f2dd50a1798c8b35ad701e5c7312
Jcarlos0828 <jose.carlos4013@gmail.com>
2025-05-24 21:53:55 -0600

    Backend routing main structure

---
+++ backend/run.py
@@ -0,0 +1,12 @@
+import os
+from app import create_app
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv() 
+app = create_app()
+CORS(app) 
+load_dotenv()
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8010)
